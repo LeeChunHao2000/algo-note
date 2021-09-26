46b29e94d0ff55e60c9e153c13d2a0f054559514
LeeChunHao2000 <meowinhere@gmail.com>
2021-09-26 19:01:15 +0800

    add: bubblesort

---
+++ bubblesort.py
@@ -0,0 +1,6 @@
+def bubblesort(data):
+    n = len(data)
+    for i in range(n-2):
+        for j in range(n-i-1):
+            if data[j] > data[j+1]:
+                data[j], data[j+1] = data[j+1], data[j]
